e7e4aab2fdbeff2d24c1919389116f84cc3da67e
Reyna May <roma.reynamayA@gmail.com>
2023-07-05 13:57:22 -0600

    Connect to the server

---
+++ play.js
@@ -0,0 +1,23 @@
+// client setup
+const net = require('net');
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243", // IP ADDRESS HERE
+    port: 50541 // PORT number here
+  });
+
+  // to handle incoming data from the server
+  conn.on("data", (data) => {
+    console.log("server says: ", data);
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
